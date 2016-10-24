f477a4fec79da0dff1c8405c6cd5470f3d49d134
Jen Bourey <jennifer.bourey@gmail.com>
2016-10-24 16:33:40 +0200

    Reorganizing

--- app/app2.module.ts
+++ app/app2.module.ts
@@ -6,6 +6,32 @@ import { UpgradeModule, downgradeComponent} from '@angular/upgrade';
 
 import { footballApp } from './app.module';
 
+// a placeholder component that acts as a root component for angular 2 modules
+@Component({
+  selector : 'ng2-router-root',
+  template: `<div class="ng2-indicator">ng2!</div><router-outlet></router-outlet>`
+})
+export class Ng2RouterRoot {}
+
+
+/** Wrapper root module for angular 1 */
+export const RootModule = angular.module('rootModule', ['ngRoute', footballApp.name]);
+
+RootModule.component('rootCmp', {template : '<div class="ng-view"></div>'});
+
+RootModule.directive('ng2RouterRoot', downgradeComponent({
+  component: Ng2RouterRoot,
+  outputs: ['ng2RouterRoot'],
+}) as ng.IDirectiveFactory);
+
+// Tell the angular 1 router to render the placeholder
+RootModule.config(($routeProvider: angular.route.IRouteProvider) => {
+  $routeProvider
+    .otherwise({template : '<ng2-router-root></ng2-router-root>',
+        reloadOnSearch: false});
+});
+
+
 // This URL handling strategy is custom and application-specific.
 // Using it we can tell the Angular 2 router to handle only specific URLs.
 class Ng1Ng2UrlHandlingStrategy implements UrlHandlingStrategy {
@@ -14,13 +40,6 @@ class Ng1Ng2UrlHandlingStrategy implements UrlHandlingStrategy {
   extract(url: any) { return url; }
   merge(url: any, whole: any) { return url; }
 }
-
-// a placeholder component that acts as a root component for angular 2 modules
-@Component({
-  selector : 'ng2-router-root', 
-  template: `<div class="ng2-indicator">ng2!</div><router-outlet></router-outlet>`
-})
-export class Ng2RouterRoot {}
 
 
 /**
@@ -41,21 +60,3 @@ export class Ng2RouterRoot {}
 export class AppModule {
   ngDoBootstrap() {}
 }
-
-
-/** Wrapper root module for angular 1 */
-export const RootModule = angular.module('rootModule', ['ngRoute', footballApp.name]);
-
-RootModule.component('rootCmp', {template : '<div class="ng-view"></div>'});
-
-RootModule.directive('ng2RouterRoot', downgradeComponent({
-  component: Ng2RouterRoot,
-  outputs: ['ng2RouterRoot'],
-}) as ng.IDirectiveFactory);
-
-// Tell the angular 1 router to render the placeholder
-RootModule.config(($routeProvider: angular.route.IRouteProvider) => {
-  $routeProvider
-    .otherwise({template : '<ng2-router-root></ng2-router-root>',
-        reloadOnSearch: false});
-});
